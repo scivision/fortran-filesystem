4ac4aad598a3a484a1c9b9aa377d63efec37afd8
Michael Hirsch <scivision@users.noreply.github.com>
2021-12-05 16:51:55 -0500

    functions into files by implementation

--- src/posix_crt.f90
+++ src/posix_crt.f90
@@ -11,9 +11,48 @@ import c_int, c_char
 character(kind=c_char), intent(in) :: path(*)
 integer(c_int), value, intent(in) :: mask
 end function mkdir_c
+
+subroutine realpath_c(path, rpath) bind(C, name="realpath")
+!! https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/realpath.3.html
+!! https://linux.die.net/man/3/realpath
+import c_char
+character(kind=c_char), intent(in) :: path(*)
+character(kind=c_char), intent(out) :: rpath(*)
+end subroutine realpath_c
 end interface
 
 contains
+
+
+module procedure canonical
+
+character(kind=c_char, len=:), allocatable :: wk
+integer, parameter :: N = 4096
+character(kind=c_char):: c_buf(N)
+character(N) :: buf
+integer :: i
+
+if(len_trim(path) == 0) error stop "cannot canonicalize empty path"
+
+wk = expanduser(path)
+
+!! some systems e.g. Intel oneAPI Windows can't handle leading "." or ".."
+!! so manually resolve this part with CWD, which is implicit.
+
+if (wk(1:1) == ".") wk = cwd() // "/" // wk
+
+if(len(wk) > N) error stop "path too long"
+
+call realpath_c(wk // c_null_char, c_buf)
+
+do i = 1,N
+  if (c_buf(i) == c_null_char) exit
+  buf(i:i) = c_buf(i)
+enddo
+
+canonical = trim(buf(:i-1))
+
+end procedure canonical
 
 
 module procedure mkdir
