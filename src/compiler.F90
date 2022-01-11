84598bcd84a91346d47f0f0bd22bcd731aa89a6b
Michael Hirsch <scivision@users.noreply.github.com>
2022-01-11 17:59:20 -0500

    remove unused FPM files

--- src/compiler.F90
+++
@@ -1,7 +0,0 @@
-#ifdef __GFORTRAN__
-  include "compiler/gcc.f90.inc"
-#elif defined(__INTEL_COMPILER)
-  include "compiler/intel.f90.inc"
-#else
-  include "compiler/unknown.f90.inc"
-#endif
--- src/os.F90
+++
@@ -1,9 +0,0 @@
-#ifdef __WIN32
-  include "windows/crt.f90.inc"
-  include "windows/path.f90.inc"
-  include "windows/sys.f90.inc"
-#else
-  include "posix/crt.f90.inc"
-  include "posix/path.f90.inc"
-  include "posix/sys.f90.inc"
-#endif
