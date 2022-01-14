b5250b0878decd52b4b9b1bfa33970bee391425e
Michael Hirsch <scivision@users.noreply.github.com>
2022-01-14 16:19:41 -0500

    test_parts: more tests

--- src/tests/test_parts.f90
+++ src/tests/test_parts.f90
@@ -24,14 +24,24 @@ if (pts(1) /= "idempotent") error stop "idempotent"
 
 p1 = path_t("a1/b23/c456")
 pts = p1%parts()
-if(size(pts) /= 3) error stop "no_startend split"
-if(len(pts(1)) /= 4) error stop "no_startend len"
-if(pts(1) /= "a1") error stop "no_startend 1: " // pts(1)
-if(pts(2) /= "b23") error stop "no_startend 2"
-if(pts(3) /= "c456") error stop "no_startend 3"
-fpts = parts("a1/b23/c456")
-if(size(fpts) /= 3) error stop "no_startend fcn split"
-if(any(fpts /= pts)) error stop "no_startend fcn /= OO"
+if(size(pts) /= 3) error stop "no_startend split %parts wrong size"
+if(len(pts(1)) /= 4) error stop "no_startend len %parts"
+if(pts(1) /= "a1") error stop "no_startend 1 %parts: " // pts(1)
+if(pts(2) /= "b23") error stop "no_startend 2 %parts: " // pts(2)
+if(pts(3) /= "c456") error stop "no_startend 3 %parts: " // pts(3)
+
+fpts = parts(p1%path())
+if(size(fpts) /= 3) error stop "no_startend fcn split wrong size"
+if(len(fpts(1)) /= 4) error stop "no_startend len parts()"
+if(fpts(1) /= "a1") error stop "no_startend 1 parts(): " // pts(1)
+if(fpts(2) /= "b23") error stop "no_startend 2 parts(): " // pts(2)
+if(fpts(3) /= "c456") error stop "no_startend 3 parts(): " // pts(3)
+
+if(any(fpts /= pts)) then
+  write(stderr,*) "ERROR: parts(): ", fpts, " /= %parts(): ", pts
+  error stop "no_startend fcn /= OO"
+endif
+
 
 p1 = path_t("/a1/b23/c456")
 pts = p1%parts()
