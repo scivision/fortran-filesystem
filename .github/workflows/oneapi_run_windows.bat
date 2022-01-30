506ee1ec08358898792acfc8a3eef19075158d25
Michael Hirsch <scivision@users.noreply.github.com>
2022-01-30 14:14:13 -0500

    oneapi:windows

---
+++ .github/workflows/oneapi_run_windows.bat
@@ -0,0 +1,43 @@
+
+REM workaround since Github Actions doesn't support shell cmd well,
+REM and cmd is needed for oneAPI Windows
+
+call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat"
+
+REM doesn't work
+@REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+@REM call "C:\Program Files (x86)\Intel\oneAPI\compiler\latest\env\vars.bat"
+
+set CC=icx
+set CXX=icx
+set FC=ifx
+
+REM configure
+cmake --preset multi
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+REM Release build
+cmake --build --preset release
+if %errorlevel% neq 0 exit /b %errorlevel%
+REM Release unit test
+ctest --preset release --schedule-random -V
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+REM debug build
+cmake --build --preset debug
+if %errorlevel% neq 0 exit /b %errorlevel%
+REM debug unit test
+ctest --preset debug --schedule-random -V
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+REM example config
+cmake -B examples/build -S examples
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+REM example build
+cmake --build examples/build
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+REM example test
+ctest --test-dir examples/build -V
+if %errorlevel% neq 0 exit /b %errorlevel%
