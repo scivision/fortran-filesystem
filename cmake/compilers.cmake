443a2259233091fb30e357c0a9dc5c420976544e
Michael Hirsch <scivision@users.noreply.github.com>
2022-01-30 11:49:54 -0500

    check Fortran 2018 support to give better feedback for old compilers

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,13 +1,46 @@
 include(CheckIncludeFileCXX)
 include(CheckCXXSymbolExists)
 include(CheckSourceRuns)
+include(CheckSourceCompiles)
+
+# check if Fortran compiler new enough
+check_source_compiles(Fortran "
+module a
+implicit none (type, external)
+
+interface
+module subroutine d()
+end subroutine d
+end interface
+end module
+
+submodule (a) b
+contains
+module procedure d
+end procedure d
+end submodule
+
+program c
+use a, only : d
+
+character :: e
+error stop e
+end program
+"
+HAS_Fortran_2018
+)
+if(NOT HAS_Fortran_2018)
+  message(FATAL_ERROR "Fortran compiler ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION} does not support Fortran 2018 syntax")
+endif()
+
+# setup / check C++ filesystem
 
 set(libfs)
 if(CMAKE_CXX_COMPILER_ID STREQUAL GNU AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9.1.0)
   set(libfs stdc++fs)
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL Clang AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9.0.0)
 # https://releases.llvm.org/9.0.0/projects/libcxx/docs/UsingLibcxx.html#using-filesystem
-  # set(libfs c++fs)  # /usr/bin/ld: cannot find -lc++fs  also happens in Meson
+  # set(libfs c++fs)
 elseif(CMAKE_CXX_COMPILER_ID MATCHES "^Intel" AND CMAKE_SYSTEM_NAME STREQUAL Linux)
   # NOTE: Intel compiler must use GCC >= 9 else you get linker errors, even with -lstdc++fs
   # e.g. on CentOS / RHEL use gcc-toolset-9 or similar
@@ -15,6 +48,7 @@ elseif(CMAKE_CXX_COMPILER_ID MATCHES "^Intel" AND CMAKE_SYSTEM_NAME STREQUAL Lin
 endif()
 
 set(CMAKE_REQUIRED_LIBRARIES ${libfs})
+set(CMAKE_REQUIRED_FLAGS -std=c++17)
 
 check_cxx_symbol_exists(__cpp_lib_filesystem filesystem HAVE_CXXFS_MACRO)
 if(HAVE_CXXFS_MACRO)
