b5ebdcc69d341d7790799b846bd9b09c0fb2d121
Michael Hirsch <scivision@users.noreply.github.com>
2022-01-31 01:21:15 -0500

    windows-oneapi ninja

--- .github/workflows/oneapi_run_windows.bat
+++ .github/workflows/oneapi_run_windows.bat
@@ -8,34 +8,34 @@ REM doesn't work
 @REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 @REM call "C:\Program Files (x86)\Intel\oneAPI\compiler\latest\env\vars.bat"
 
-REM configure
+echo "configure"
 cmake --preset multi
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-REM Release build
+echo "Release build"
 cmake --build --preset release
 if %errorlevel% neq 0 (
   type build\CMakeFiles\CMakeError.log & exit /b %errorlevel%
 )
-REM Release unit test
+echo "Release unit test"
 ctest --preset release --schedule-random -V
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-REM debug build
+echo "Debug build"
 cmake --build --preset debug
 if %errorlevel% neq 0 exit /b %errorlevel%
-REM debug unit test
+echo "Debug unit test"
 ctest --preset debug --schedule-random -V
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-REM example config
+echo "Example config"
 cmake -B examples/build -S examples
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-REM example build
+echo "Example build"
 cmake --build examples/build
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-REM example test
+echo "Example test"
 ctest --test-dir examples/build -V
 if %errorlevel% neq 0 exit /b %errorlevel%
