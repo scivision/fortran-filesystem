f6a284b594f6abe3dd46bd7ddb02c1bf2af79712
Michael Hirsch <scivision@users.noreply.github.com>
2022-03-23 11:36:41 -0400

    update syntax

--- cmake/ffilesystem.cmake
+++ cmake/ffilesystem.cmake
@@ -11,24 +11,21 @@ endif()
 
 set(ffilesystem_external true CACHE BOOL "Fortran Filesystem autobuild")
 
-if(NOT ffilesystem_ROOT)
-  set(ffilesystem_ROOT ${CMAKE_INSTALL_PREFIX})
-endif()
 
 if(BUILD_SHARED_LIBS)
   if(WIN32)
-    set(ffilesystem_LIBRARIES ${ffilesystem_ROOT}/bin/${CMAKE_SHARED_LIBRARY_PREFIX}filesystem${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(ffilesystem_LIBRARIES ${CMAKE_INSTALL_PREFIX}/bin/${CMAKE_SHARED_LIBRARY_PREFIX}filesystem${CMAKE_SHARED_LIBRARY_SUFFIX})
   else()
-    set(ffilesystem_LIBRARIES ${ffilesystem_ROOT}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}filesystem${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(ffilesystem_LIBRARIES ${CMAKE_INSTALL_PREFIX}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}filesystem${CMAKE_SHARED_LIBRARY_SUFFIX})
   endif()
 else()
-  set(ffilesystem_LIBRARIES ${ffilesystem_ROOT}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}filesystem${CMAKE_STATIC_LIBRARY_SUFFIX})
+  set(ffilesystem_LIBRARIES ${CMAKE_INSTALL_PREFIX}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}filesystem${CMAKE_STATIC_LIBRARY_SUFFIX})
 endif()
 
-set(ffilesystem_INCLUDE_DIRS ${ffilesystem_ROOT}/include)
+set(ffilesystem_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include)
 
 set(ffilesystem_cmake_args
--DCMAKE_INSTALL_PREFIX=${ffilesystem_ROOT}
+-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
 -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
 -DCMAKE_BUILD_TYPE=Release
 -DBUILD_TESTING:BOOL=false
@@ -41,7 +38,7 @@ ExternalProject_Add(FFILESYSTEM
 SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/..
 CMAKE_ARGS ${ffilesystem_cmake_args}
 CMAKE_GENERATOR ${EXTPROJ_GENERATOR}
-BUILD_BYPRODUCTS ${ffilesystem_LIBRARIES} ${ffs_implib}
+BUILD_BYPRODUCTS ${ffilesystem_LIBRARIES}
 INACTIVITY_TIMEOUT 15
 CONFIGURE_HANDLED_BY_BUILD true
 )
@@ -52,7 +49,7 @@ file(MAKE_DIRECTORY ${ffilesystem_INCLUDE_DIRS})
 add_library(ffilesystem::filesystem INTERFACE IMPORTED)
 
 target_link_libraries(ffilesystem::filesystem INTERFACE ${ffilesystem_LIBRARIES} ${lib_filesystem})
-target_include_directories((ffilesystem::filesystem INTERFACE ${ffilesystem_INCLUDE_DIRS})
+target_include_directories(ffilesystem::filesystem INTERFACE ${ffilesystem_INCLUDE_DIRS})
 set_target_properties(ffilesystem::filesystem PROPERTIES LINKER_LANGUAGE CXX)
 # target_link_libraries(ffilesystem::filesystem INTERFACE stdc++)  # did not help
 # instead, set linker_langauge CXX for the specific targets linking ffilesystem::filesystem
