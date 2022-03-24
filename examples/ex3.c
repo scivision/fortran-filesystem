31e2ed5757bca792b3a1d20de681d6cf20da1937
Michael Hirsch <scivision@users.noreply.github.com>
2022-03-24 14:38:20 -0400

    make header also C compatible

---
+++ examples/ex3.c
@@ -0,0 +1,21 @@
+// use ffilesystem library from C
+
+#include <stdio.h>
+
+#include "filesystem.h"
+
+int main(void) {
+
+  char d[4096];
+
+  get_cwd(d);
+  printf("current working dir %s\n", d);
+
+  get_homedir(d);
+  printf("home dir %s\n", d);
+
+  expanduser("~", d);
+  printf("expanduser('~') %s\n", d);
+
+  return 0;
+}
