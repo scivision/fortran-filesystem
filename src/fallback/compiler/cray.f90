e5dd7ca6e9233253861e243973047d4d46162a8b
Michael Hirsch <scivision@users.noreply.github.com>
2022-04-20 12:05:39 -0400

    fallback:cray: better is_dir heuristic [skip ci]

--- src/fallback/compiler/cray.f90
+++ src/fallback/compiler/cray.f90
@@ -2,7 +2,10 @@ submodule (filesystem) cray_no_cpp_fs
 !! Cray external procedures
 !! Cray Fortran has stat() subroutine but unsure of API.
 
+use, intrinsic :: iso_fortran_env, only : int8
+
 implicit none (type, external)
+
 
 contains
 
@@ -25,6 +28,7 @@ end procedure get_cwd
 module procedure is_dir
 
 integer :: u, ierr
+integer(int8) :: x
 character(:), allocatable :: wk
 
 is_dir = .false.
@@ -35,9 +39,12 @@ if(len_trim(wk) == 0) return
 inquire(file=wk, exist=is_dir)
 if(.not.is_dir) return
 
-!> heuristic: try to open, if fail, then assume it's a directory
-!> this obviously has many incorrect corner cases, but it's a start
-open(newunit=u, file=wk, action='read', status='old', iostat=ierr)
+!> heuristic: try to open, and read. If fail, assume it's a directory
+!> this has incorrect corner cases, but it's a start
+open(newunit=u, file=wk, action='read', access='stream', status='old', iostat=ierr)
+is_dir = ierr /= 0
+if(is_dir) return
+read(u, iostat=ierr) x
 is_dir = ierr /= 0
 close(u)
 
