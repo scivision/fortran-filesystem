a8e07b5a78ddb6dc4996ff7564ebbb639a5c0473
Michael Hirsch <scivision@users.noreply.github.com>
2022-04-25 13:42:07 -0400

    cray: parameterize

--- cray.cmake
+++ cray.cmake
@@ -7,15 +7,26 @@
 # NOTE: to specify install directory, run like:
 #   cmake -DCMAKE_INSTALL_PREFIX=<install_dir> -P cray.cmake
 
-cmake_minimum_required(VERSION 3.20...3.23)
+cmake_minimum_required(VERSION 3.20)
+
+# --- user options
 
 option(intel "use intel compiler instead of default GCC")
+
+# --- default params
+
+cmake_path(SET gcc_dir /opt/cray/pe/gcc/11.2.0/snos)
+set(cpe cpe/22.03)
+set(mpi cray-mpich)
+set(oneapi intel-oneapi/2022.0.2)
+
+# --- main script
 
 # the module commands only affect the current process, not the parent shell
 if(intel)
   cmake_path(SET BINARY_DIR ${CMAKE_CURRENT_LIST_DIR}/build-intel)
   if(NOT DEFINED ENV{CXXFLAGS})
-    set(ENV{CXXFLAGS} --gcc-toolchain=/opt/cray/pe/gcc/11.2.0/snos)
+    set(ENV{CXXFLAGS} --gcc-toolchain=${gcc_dir})
   endif()
   # GCC >= 9.1 must be used for C++17 filesystem
 else()
@@ -31,13 +42,15 @@ endif()
 
 find_package(EnvModules REQUIRED)
 
-env_module(load cpe/22.03 OUTPUT_VARIABLE out RESULT_VARIABLE ret)
-if(ret)
-  message(STATUS "load cpe/22.03 error ${ret}: ${out}")
+if(cpe)
+  env_module(load ${cpe} OUTPUT_VARIABLE out RESULT_VARIABLE ret)
+  if(ret)
+    message(STATUS "load ${cpe} error ${ret}: ${out}")
+  endif()
 endif()
 
 if(intel)
-  env_module(load intel-oneapi/2022.0.2 OUTPUT_VARIABLE out RESULT_VARIABLE ret)
+  env_module(load ${oneapi} OUTPUT_VARIABLE out RESULT_VARIABLE ret)
   env_module_swap(PrgEnv-cray PrgEnv-intel OUTPUT_VARIABLE out RESULT_VARIABLE ret)
 else()
   env_module_swap(PrgEnv-cray PrgEnv-gnu OUTPUT_VARIABLE out RESULT_VARIABLE ret)
@@ -55,9 +68,9 @@ env_module(load python OUTPUT_VARIABLE out RESULT_VARIABLE ret)
 # endif()
 
 if(NOT intel)
-  env_module(load cray-mpich OUTPUT_VARIABLE out RESULT_VARIABLE ret)
+  env_module(load ${mpi} OUTPUT_VARIABLE out RESULT_VARIABLE ret)
   if(ret)
-    message(STATUS "load cray-mpich error ${ret}: ${out}")
+    message(STATUS "load ${mpi} error ${ret}: ${out}")
   endif()
 endif()
 
