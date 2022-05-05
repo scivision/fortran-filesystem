ccf26aefe693a3adda81be5356db0f1e6833a32a
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-05 13:48:43 -0400

    add test c++ interface

---
+++ tests/test_cpp_ifc.cpp
@@ -0,0 +1,28 @@
+// use ffilesystem library from C++
+
+#include <iostream>
+
+#if __has_include(<filesystem>)
+#include <filesystem>
+namespace fs = std::filesystem;
+#elif __has_include(<experimental/filesystem>)
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#else
+#error "No C++ filesystem support"
+#endif
+
+#include "filesystem.h"
+
+int main() {
+
+  char fpath[4096];
+
+  get_cwd(fpath);
+  std::cout << "Fortran: current working dir " << fpath << std::endl;
+
+  auto cwd = fs::current_path();
+  std::cout << "C++: current working dir " << cwd << std::endl;
+
+  return EXIT_SUCCESS;
+}
