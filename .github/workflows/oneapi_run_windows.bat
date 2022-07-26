60edf79ceacd883115306f8c640dfdec8a61a45e
Michael Hirsch <scivision@users.noreply.github.com>
2022-07-26 12:01:14 -0400

    ci:oneapi:windows update to 2022.2

--- .github/workflows/oneapi_run_windows.bat
+++ .github/workflows/oneapi_run_windows.bat
@@ -1,41 +1,42 @@
-
-REM workaround since Github Actions doesn't support shell cmd well,
+REM batch file since Github Actions doesn't support shell cmd well,
 REM and cmd is needed for oneAPI Windows
 
 call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat"
 
-REM doesn't work
-@REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-@REM call "C:\Program Files (x86)\Intel\oneAPI\compiler\latest\env\vars.bat"
-
 echo "configure"
-cmake --preset multi
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-echo "Release build"
-cmake --build --preset release
+cmake --preset multi -DCMAKE_INSTALL_PREFIX=%RUNNER_TEMP%
 if %errorlevel% neq 0 (
   type build\CMakeFiles\CMakeError.log & exit /b %errorlevel%
 )
+
+echo "Release build"
+cmake --build --preset release
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 echo "Release unit test"
 ctest --preset release --schedule-random -V
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo "install project"
+cmake --install build
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo "Debug build"
 cmake --build --preset debug
 if %errorlevel% neq 0 exit /b %errorlevel%
+
 echo "Debug unit test"
 ctest --preset debug --schedule-random -V
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo "Example config"
-cmake -B examples/build -S examples
+cmake -B example/build -S example -DCMAKE_PREFIX_PATH=%RUNNER_TEMP%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo "Example build"
-cmake --build examples/build
+cmake --build example/build
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo "Example test"
-ctest --test-dir examples/build -V
+ctest --test-dir example/build -V
 if %errorlevel% neq 0 exit /b %errorlevel%
