989889688a4880b934d308cf358f53271dbfddde
Michael Hirsch <scivision@users.noreply.github.com>
2022-08-18 09:55:33 -0400

    set path_t maximum per maxp at initialization of object

--- src/filesystem.f90
+++ src/filesystem.f90
@@ -375,10 +375,11 @@ contains
 
 !! non-functional API
 
-pure function set_path(path)
+function set_path(path)
 type(path_t) :: set_path
 character(*), intent(in) :: path
-set_path%path_str = trim(path)
+allocate(character(get_max_path()) :: set_path%path_str)
+set_path%path_str = path
 end function set_path
 
 
@@ -388,11 +389,17 @@ class(path_t), intent(in) :: self
 integer, intent(in), optional :: istart, iend
 integer :: i1, i2
 
-i1 = 1
-i2 = len_trim(self%path_str)
+if(present(istart)) then
+  i1 = istart
+else
+  i1 = 1
+endif
 
-if(present(istart)) i1 = istart
-if(present(iend)) i2 = iend
+if(present(iend)) then
+  i2 = iend
+else
+  i2 = len_trim(self%path_str)
+endif
 
 get_path = self%path_str(i1:i2)
 
