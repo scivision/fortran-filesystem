0b91cc5846dcf73b7730b0a07cac58c5b23bbce7
Michael Hirsch <scivision@users.noreply.github.com>
2022-09-16 12:57:55 -0400

    concepts\lib_dir: oneapi: programmatic dll name [skip ci]

--- concepts/lib_dir/lib.c
+++ concepts/lib_dir/lib.c
@@ -1,17 +1,21 @@
 #include <string.h>
 
 #ifdef _MSC_VER
+#ifndef MY_DLL_NAME
+#error "must define MY_DLL_PATH with filename of this file's DLL"
+#endif
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #else
 #include <dlfcn.h>
 #endif
 
+
 size_t get_libpath(char* path)
 {
 
 #ifdef _MSC_VER
-  if (GetModuleFileName(GetModuleHandle("mylib.dll"), path, MAX_PATH) !=0)
+  if (GetModuleFileName(GetModuleHandle(MY_DLL_NAME), path, MAX_PATH) !=0)
     return strlen(path);
 #else
  Dl_info info;
