93f708728dce7fff8f821ac682e2eff5ebcc1d96
Michael Hirsch <scivision@users.noreply.github.com>
2022-10-06 11:55:02 -0400

    default build shared (enables lib_path, lib_dir)

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -3,7 +3,7 @@ message(STATUS "${PROJECT_NAME} ${PROJECT_VERSION} CMake ${CMAKE_VERSION} Toolch
 option(cpp "Use C++ filesystem for full functionality" on)
 option(fortran "use the Fortran interaces to C functions" on)
 option(cli "Build CLI" on)
-option(BUILD_SHARED_LIBS "Build shared libraries")
+option(BUILD_SHARED_LIBS "Build shared libraries" on)
 
 option(coverage "Code coverage tests")
 option(tidy "Run clang-tidy on the code")
