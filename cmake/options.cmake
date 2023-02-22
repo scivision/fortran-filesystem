6eab0c2f2102de01492c65f20d2973328cbea7b4
scivision <scivision@users.noreply.github.com>
2023-02-22 01:54:50 -0500

    better CMake .gitignore

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -19,7 +19,4 @@ set(CMAKE_INSTALL_RPATH_USE_LINK_PATH true)
 # Necessary for shared library with Visual Studio / Windows oneAPI
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
 
-# --- auto-ignore build directory
-if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
-  file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
-endif()
+file(GENERATE OUTPUT .gitignore CONTENT "*")
