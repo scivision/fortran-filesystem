9ff91e2a91d3eccd59af8fd22507414342dd5cc3
scivision <scivision@users.noreply.github.com>
2023-04-01 22:27:37 -0400

    c: simplify logic

--- src/c/env.c
+++ src/c/env.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -16,18 +15,12 @@ static size_t fs_getenv(const char* name, char* path, size_t buffer_size)
   // <stdlib.h>
   char* buf = getenv(name);
   if(!buf) // not error because sometimes we just check if envvar is defined
-    goto retnull;
+    return 0;
 
-  if(strlen(buf) >= buffer_size){
-    fprintf(stderr, "ERROR:ffilesystem:getenv: buffer too small\n");
-    goto retnull;
-  }
+  if(strlen(buf) >= buffer_size)
+    buf[buffer_size-1] = '\0';
 
   return fs_normal(buf, path, buffer_size);
-
-retnull:
-  path = NULL;
-  return 0;
 }
 
 
@@ -37,46 +30,30 @@ size_t fs_get_cwd(char* path, size_t buffer_size)
   // <direct.h> / <unistd.h>
   char* x = getcwd(path, buffer_size);
 
-  if(!x || strlen(x) >= buffer_size){
-    fprintf(stderr, "ERROR:ffilesystem:getcwd\n");
-    goto nullret;
-  }
+  if(!x)
+    return 0;
+
+  if(strlen(x) >= buffer_size)
+    path[buffer_size-1] = '\0';
 
   return fs_normal(path, path, buffer_size);
-
-nullret:
-  path = NULL;
-  return 0;
 }
 
 size_t fs_get_homedir(char* path, size_t buffer_size)
 {
-  if(fs_is_windows()){
-    return fs_getenv("USERPROFILE", path, buffer_size);
-  }
-  else{
-    return fs_getenv("HOME", path, buffer_size);
-  }
+  return fs_getenv(fs_is_windows() ? "USERPROFILE" : "HOME", path, buffer_size);
 }
 
 size_t fs_get_tempdir(char* path, size_t buffer_size)
 {
-  size_t L;
-  if(fs_is_windows()){
-    L = fs_getenv("TEMP", path, buffer_size);
-  }
-  else{
-    L = fs_getenv("TMPDIR", path, buffer_size);
-  }
-  if(L > 0)
+  size_t L = fs_getenv(fs_is_windows() ? "TEMP" : "TMPDIR", path, buffer_size);
+  if(L)
     return L;
 
-  if (fs_is_dir("/tmp") && buffer_size > 4){
+  if (buffer_size > 4 && fs_is_dir("/tmp")){
     strcpy(path, "/tmp");
     return 4;
   }
 
-  fprintf(stderr, "ERROR:ffilesystem:get_tempdir: could not find temp dir\n");
-  path = NULL;
   return 0;
 }
--- src/c/get_path.c
+++ src/c/get_path.c
@@ -5,7 +5,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <stdio.h>
 
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
@@ -25,6 +24,10 @@ static void dl_dummy_func() {}
 #include <unistd.h>
 #endif
 
+#ifndef min
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#endif
+
 
 size_t fs_exe_path(char* path, size_t buffer_size)
 {
@@ -33,17 +36,19 @@ size_t fs_exe_path(char* path, size_t buffer_size)
 
 #ifdef _WIN32
  // https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulefilenamea
-  if (GetModuleFileName(NULL, path, (DWORD)buffer_size) == 0) goto retnull;
+  return GetModuleFileName(NULL, path, (DWORD)buffer_size);
 #elif defined(__linux__) || defined(__CYGWIN__)
   // https://man7.org/linux/man-pages/man2/readlink.2.html
   size_t L = readlink("/proc/self/exe", path, buffer_size);
-  if (L < 1 || L >= buffer_size) goto retnull;
+  if (L < 1)
+    return 0;
+  if (L >= buffer_size)
+    L = buffer_size-1;
   path[L] = '\0';
 #elif defined(__APPLE__)
   char buf[buffer_size];
   uint32_t mp = sizeof(buf);
-  if (_NSGetExecutablePath(buf, &mp) != 0) goto retnull;
-  if (!realpath(buf, path)) return 0;
+  if (_NSGetExecutablePath(buf, &mp) || !realpath(buf, path)) return 0;
 #elif defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
   char* buf = (char*) malloc(buffer_size);
   int mib[4];
@@ -53,24 +58,16 @@ size_t fs_exe_path(char* path, size_t buffer_size)
   mib[3] = -1;
   size_t cb = sizeof(buf);
 
-  if(sysctl(mib, 4, buf, &cb, NULL, 0) != 0){
+  if(sysctl(mib, 4, buf, &cb, NULL, 0) || !realpath(buf, path)){
     free(buf);
-    goto retnull;
-  }
-  if(!realpath(buf, path)){
-    free(buf);
-    goto retnull;
+    return 0
   }
   free(buf);
 #else
-  goto retnull;
+  return 0;
 #endif
 
   return strlen(path);
-
-retnull:
-  path = NULL;
-  return 0;
 }
 
 
@@ -79,30 +76,22 @@ size_t fs_lib_path(char* path, size_t buffer_size)
 
 #if (defined(_WIN32) || defined(__CYGWIN__)) && defined(FS_DLL_NAME)
  // https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulefilenamea
-  if(GetModuleFileName(GetModuleHandle(FS_DLL_NAME), path, (DWORD)buffer_size) == 0)
-    goto retnull;
+  return GetModuleFileName(GetModuleHandle(FS_DLL_NAME), path, (DWORD)buffer_size);
 #elif defined(HAVE_DLADDR)
   Dl_info info;
 
-  if (dladdr( (void*)&dl_dummy_func, &info) == 0) goto retnull;
+  if (!dladdr( (void*)&dl_dummy_func, &info))
+    return 0;
 
   size_t L = strlen(info.dli_fname);
+  size_t N = min(L, buffer_size-1);
 
-  if(L >= buffer_size){
-    fprintf(stderr, "ERROR:filesystem:fs_lib_path: buffer too small\n");
-    goto retnull;
-  }
-
-  strncpy(path, info.dli_fname, buffer_size);
-  path[L] = '\0';
-#else
-  goto retnull;
+  strncpy(path, info.dli_fname, N);
+  path[N+1] = '\0';
+  return N;
 #endif
 
-  return strlen(path);
-
-retnull:
-  path = NULL;
+  (void) path;
   return 0*buffer_size;
   // to avoid unused argument error
 }
