0a8f26e2ef172a4675915b483e04ba38aab9ea7f
scivision <scivision@users.noreply.github.com>
2023-07-16 18:42:25 -0400

    default BUILD_SHARED_LIBS=off (static) to improve robustness

--- options.cmake
+++ options.cmake
@@ -3,7 +3,7 @@ message(STATUS "${PROJECT_NAME} ${PROJECT_VERSION} CMake ${CMAKE_VERSION} ${CMAK
 option(cpp "Use C++ filesystem for full functionality" on)
 option(fortran "use the Fortran interaces to C functions" on)
 option(cli "Build CLI" on)
-option(BUILD_SHARED_LIBS "Build shared libraries" on)
+option(BUILD_SHARED_LIBS "Build shared libraries")
 option(fallback "Fallback to non-C++ filesystem.c if C++ stdlib is not working" on)
 
 option(coverage "Code coverage tests")
