6791531f14351eaf160370d76821b5073e05aedf
Michael Hirsch <scivision@users.noreply.github.com>
2023-11-19 14:05:48 -0500

    add UTF8 tests

---
+++ test/c/test_utf8.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <exception>
+#include <cstdlib>
+
+#include "ffilesystem.h"
+
+int main()
+{
+
+std::string smiley = "😀", wink = "😉", hello = "你好";
+
+std::string u1;
+
+for (auto u : {smiley, wink, hello}) {
+  u1 = fs_file_name("./" + u);
+  if (u1 != u)
+    throw std::runtime_error("fs_file_name(./" + smiley + ") != " + u1 + " " + u);
+
+
+  u1 = fs_canonical(u, false);
+  std::cout << "canonical(" + u + "): " << u1 << "\n";
+  if (u1 != u)
+    throw std::runtime_error("canonical UTF8: "  + u1 + " " + u);
+}
+
+return EXIT_SUCCESS;
+}
---
+++ test/fortran/test_utf8.f90
@@ -0,0 +1,46 @@
+program utf8_text
+
+use filesystem
+
+implicit none
+
+call test_utf8()
+
+contains
+
+subroutine test_utf8()
+
+character(4), parameter :: smiley = "😀", wink = "😉"
+character(6), parameter :: hello = "你好"
+
+character(4) :: u1
+character(6) :: u2
+
+u1 = file_name("./" // smiley)
+print '(a)', u1
+if (u1 /= smiley) error stop "ERROR: filename UTF8 smiley: " // u1 // " " // smiley
+
+u1 = file_name("./" // wink)
+print '(a)', u1
+if (u1 /= wink) error stop "ERROR: filename UTF8 wink: " // u1 // " " // wink
+
+u2 = file_name("./" // hello)
+print '(a)', u2
+if (u2 /= hello) error stop "ERROR: filename UTF8 hello: " // u2 // " " // hello
+
+
+u1 = canonical(smiley)
+print '(a)', "canonical(" // smiley // ")" // " " // u1
+if (u1 /= smiley) error stop "ERROR: canonical UTF8 smiley: " // u1 // " "// smiley
+
+u1 = canonical(wink)
+print '(a)', "canonical(" // wink // ")" // " " // u1
+if (u1 /= wink) error stop "ERROR: canonical UTF8 wink: " // u1 // " " // wink
+
+u2 = canonical(hello)
+print '(a)', "canonical(" // hello // ")" // " " // u2
+if (u2 /= hello) error stop "ERROR: canonical UTF8 hello: " // u2 // " " // hello
+
+end subroutine
+
+end program
