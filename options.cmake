a55662eb0c21fe70900496443fe94eb37e54e5a5
scivision <scivision@users.noreply.github.com>
2023-11-20 23:06:38 -0500

    cmake: print CXX_STANDARD

--- options.cmake
+++ options.cmake
@@ -1,5 +1,3 @@
-message(STATUS "${PROJECT_NAME} ${PROJECT_VERSION} CMake ${CMAKE_VERSION} ${CMAKE_SYSTEM_NAME} Toolchain ${CMAKE_TOOLCHAIN_FILE}")
-
 option(cpp "Use C++ filesystem for full functionality" on)
 option(fortran "use the Fortran interaces to C functions" on)
 option(cli "Build CLI" on)
