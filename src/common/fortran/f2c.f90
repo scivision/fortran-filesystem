f4869df8e52049e84c86c8a350ac6b64d9d0423e
scivision <scivision@users.noreply.github.com>
2024-02-14 10:19:25 -0500

    consolidate get_path.f90

--- src/common/fortran/f2c.f90
+++ src/common/fortran/f2c.f90
@@ -286,6 +286,18 @@ character(kind=C_CHAR), intent(out) :: out(*)
 integer(C_SIZE_T), intent(in), value :: buffer_size
 end function
 
+integer(C_SIZE_T) function fs_lib_path(path, buffer_size) bind(C)
+import
+character(kind=C_CHAR), intent(out) :: path(*)
+integer(C_SIZE_T), intent(in), value :: buffer_size
+end function
+
+integer(C_SIZE_T) function fs_exe_path(path, buffer_size) bind(C)
+import
+character(kind=C_CHAR), intent(out) :: path(*)
+integer(C_SIZE_T), intent(in), value :: buffer_size
+end function
+
 end interface
 
 contains
@@ -660,6 +672,24 @@ allocate(character(max_path()) :: cbuf)
 N = fs_short2long(trim(path) // C_NULL_CHAR, cbuf, len(cbuf, kind=C_SIZE_T))
 allocate(character(N) :: r)
 r = cbuf(:N)
+end procedure
+
+module procedure exe_path
+character(kind=C_CHAR, len=:), allocatable :: cbuf
+integer(C_SIZE_T) :: N
+allocate(character(max_path()) :: cbuf)
+N = fs_exe_path(cbuf, len(cbuf, kind=C_SIZE_T))
+allocate(character(N) :: exe_path)
+exe_path = cbuf(:N)
+end procedure
+
+module procedure lib_path
+character(kind=C_CHAR, len=:), allocatable :: cbuf
+integer(C_SIZE_T) :: N
+allocate(character(max_path()) :: cbuf)
+N = fs_lib_path(cbuf, len(cbuf, kind=C_SIZE_T))
+allocate(character(N) :: lib_path)
+lib_path = cbuf(:N)
 end procedure
 
 
