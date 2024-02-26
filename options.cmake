b2afada6aed6539be737fda4dc0172a4ae2ed099
scivision <scivision@users.noreply.github.com>
2024-02-26 15:14:06 -0500

    cli: default build if project is top level

--- options.cmake
+++ options.cmake
@@ -1,6 +1,6 @@
 option(ffilesystem_cpp "Use C++ filesystem for full functionality" on)
 option(ffilesystem_fortran "use the Fortran interaces to C functions" on)
-option(ffilesystem_cli "Build CLI" on)
+option(ffilesystem_cli "Build CLI" ${PROJECT_IS_TOP_LEVEL})
 option(ffilesystem_fallback "Fallback to non-C++ filesystem.c if C++ stdlib is not working")
 option(ffilesystem_trace "debug trace output" off)
 
