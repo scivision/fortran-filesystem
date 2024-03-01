091585d57dcaaddc9f85d24baf5953cabb9d0bc0
scivision <scivision@users.noreply.github.com>
2024-03-01 15:28:30 -0500

    header: remove implicit extern

--- include/ffilesystem.h
+++ include/ffilesystem.h
@@ -125,87 +125,87 @@ extern "C" {
 #endif
 
 
-extern bool fs_cpp();
-extern long fs_lang();
-extern size_t fs_get_max_path();
+bool fs_cpp();
+long fs_lang();
+size_t fs_get_max_path();
 
-extern char fs_pathsep();
+char fs_pathsep();
 
-extern bool fs_is_admin();
-extern bool fs_is_bsd();
-extern bool fs_is_macos();
-extern bool fs_is_linux();
-extern bool fs_is_unix();
-extern bool fs_is_windows();
-extern int fs_is_wsl();
-extern bool fs_is_mingw();
-extern bool fs_is_cygwin();
+bool fs_is_admin();
+bool fs_is_bsd();
+bool fs_is_macos();
+bool fs_is_linux();
+bool fs_is_unix();
+bool fs_is_windows();
+int fs_is_wsl();
+bool fs_is_mingw();
+bool fs_is_cygwin();
 
-extern void fs_as_posix(char*);
-extern void fs_as_windows(char*);
+void fs_as_posix(char*);
+void fs_as_windows(char*);
 
-extern size_t fs_normal(const char*, char*, size_t);
+size_t fs_normal(const char*, char*, size_t);
 
-extern size_t fs_join(const char*, const char*, char*, size_t);
-extern size_t fs_make_absolute(const char*, const char*, char*, size_t);
+size_t fs_join(const char*, const char*, char*, size_t);
+size_t fs_make_absolute(const char*, const char*, char*, size_t);
 
-extern size_t fs_file_name(const char*, char*, size_t);
-extern size_t fs_stem(const char*, char*, size_t);
-extern size_t fs_parent(const char*, char*, size_t);
-extern size_t fs_suffix(const char*, char*, size_t);
-extern size_t fs_root(const char*, char*, size_t);
+size_t fs_file_name(const char*, char*, size_t);
+size_t fs_stem(const char*, char*, size_t);
+size_t fs_parent(const char*, char*, size_t);
+size_t fs_suffix(const char*, char*, size_t);
+size_t fs_root(const char*, char*, size_t);
 
-extern size_t fs_with_suffix(const char*, const char*, char*, size_t);
+size_t fs_with_suffix(const char*, const char*, char*, size_t);
 
-extern size_t fs_which(const char*, char*, size_t);
+size_t fs_which(const char*, char*, size_t);
 
-extern bool fs_is_symlink(const char*);
-extern bool fs_create_symlink(const char*, const char*);
-extern size_t fs_read_symlink(const char*, char*, size_t);
+bool fs_is_symlink(const char*);
+bool fs_create_symlink(const char*, const char*);
+size_t fs_read_symlink(const char*, char*, size_t);
 
-extern bool fs_create_directories(const char*);
-extern bool fs_exists(const char*);
+bool fs_create_directories(const char*);
+bool fs_exists(const char*);
 
-extern bool fs_is_absolute(const char*);
-extern bool fs_is_char_device(const char*);
-extern bool fs_is_dir(const char*);
-extern bool fs_is_file(const char*);
-extern bool fs_is_exe(const char*);
-extern bool fs_is_readable(const char*);
-extern bool fs_is_writable(const char*);
-extern bool fs_is_reserved(const char*);
-extern bool fs_is_subdir(const char*, const char*);
+bool fs_is_absolute(const char*);
+bool fs_is_char_device(const char*);
+bool fs_is_dir(const char*);
+bool fs_is_file(const char*);
+bool fs_is_exe(const char*);
+bool fs_is_readable(const char*);
+bool fs_is_writable(const char*);
+bool fs_is_reserved(const char*);
+bool fs_is_subdir(const char*, const char*);
 
-extern bool fs_set_permissions(const char*, int, int, int);
+bool fs_set_permissions(const char*, int, int, int);
 
-extern size_t fs_get_permissions(const char*, char*, size_t);
+size_t fs_get_permissions(const char*, char*, size_t);
 
-extern bool fs_remove(const char*);
+bool fs_remove(const char*);
 
-extern size_t fs_canonical(const char*, bool, char*, size_t);
-extern size_t fs_resolve(const char*, bool, char*, size_t);
+size_t fs_canonical(const char*, bool, char*, size_t);
+size_t fs_resolve(const char*, bool, char*, size_t);
 
-extern bool fs_equivalent(const char*, const char*);
-extern bool fs_copy_file(const char*, const char*, bool);
-extern size_t fs_relative_to(const char*, const char*, char*, size_t);
-extern bool fs_touch(const char*);
+bool fs_equivalent(const char*, const char*);
+bool fs_copy_file(const char*, const char*, bool);
+size_t fs_relative_to(const char*, const char*, char*, size_t);
+bool fs_touch(const char*);
 
-extern size_t fs_get_cwd(char*, size_t);
-extern bool fs_set_cwd(const char*);
-extern size_t fs_get_homedir(char*, size_t);
-extern size_t fs_get_tempdir(char*, size_t);
+size_t fs_get_cwd(char*, size_t);
+bool fs_set_cwd(const char*);
+size_t fs_get_homedir(char*, size_t);
+size_t fs_get_tempdir(char*, size_t);
 
-extern size_t fs_expanduser(const char*, char*, size_t);
+size_t fs_expanduser(const char*, char*, size_t);
 
-extern uintmax_t fs_file_size(const char*);
-extern uintmax_t fs_space_available(const char*);
+uintmax_t fs_file_size(const char*);
+uintmax_t fs_space_available(const char*);
 
-extern size_t fs_exe_path(char*, size_t);
-extern size_t fs_exe_dir(char*, size_t);
-extern size_t fs_lib_path(char*, size_t);
-extern size_t fs_lib_dir(char*, size_t);
+size_t fs_exe_path(char*, size_t);
+size_t fs_exe_dir(char*, size_t);
+size_t fs_lib_path(char*, size_t);
+size_t fs_lib_dir(char*, size_t);
 
-extern size_t fs_compiler(char*, size_t);
+size_t fs_compiler(char*, size_t);
 
 bool fs_win32_is_symlink(const char*);
 
