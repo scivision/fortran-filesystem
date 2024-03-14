e9619dc4eb24ae39608e864623551d39582c2626
scivision <scivision@users.noreply.github.com>
2024-03-14 13:35:07 -0400

    auto-detect fortran

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -49,59 +49,8 @@ endif()
 
 if(HAVE_CXX_FILESYSTEM)
 
-if(NOT DEFINED ${PROJECT_NAME}_abi_ok)
-  message(CHECK_START "checking that compilers can link C++ Filesystem together")
-  try_compile(${PROJECT_NAME}_abi_ok
-  ${CMAKE_CURRENT_BINARY_DIR}/fs_check ${CMAKE_CURRENT_LIST_DIR}/fs_check
-  fs_check
-  CMAKE_FLAGS -DGNU_stdfs=${GNU_stdfs} -Dffilesystem_fortran:BOOL=${ffilesystem_fortran}
-  )
-  if(${PROJECT_NAME}_abi_ok)
-    message(CHECK_PASS "OK")
-  else()
-    message(CHECK_FAIL "Failed")
-    message(WARNING "Disabling C++ filesystem due to ABI-incompatible compilers")
-    set(HAVE_CXX_FILESYSTEM false CACHE BOOL "ABI problem with C++ filesystem" FORCE)
-  endif()
-endif()
-
-if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.25 AND ffilesystem_fortran)
-  message(CHECK_START "checking that C++ exception handling works")
-  try_compile(exception_compile
-    PROJECT exception
-    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/exception_check
-    OUTPUT_VARIABLE abi_output
-  )
-  if(abi_output MATCHES "ld: warning: could not create compact unwind for")
-    message(CHECK_FAIL "no")
-    set(HAVE_CXX_TRYCATCH false CACHE BOOL "C++ exception handling broken")
-  else()
-    message(CHECK_PASS "yes")
-    set(HAVE_CXX_TRYCATCH true CACHE BOOL "C++ exception handling works")
-  endif()
-endif()
-
-if(MINGW AND NOT DEFINED ${PROJECT_NAME}_symlink_code)
-  message(CHECK_START "check if MinGW C++ filesystem support symlink")
-  set(_symlink ${CMAKE_CURRENT_BINARY_DIR}/symlink_check)
-  set(_dummy ${_symlink}/dummy_tgt)
-  file(MAKE_DIRECTORY ${_symlink})
-  file(TOUCH ${_dummy})
-
-  try_run(${PROJECT_NAME}_symlink_code ${PROJECT_NAME}_symlink_build ${_symlink}
-        SOURCES ${CMAKE_CURRENT_LIST_DIR}/fs_check/check_fs_symlink.cpp
-        LINK_LIBRARIES "${GNU_stdfs}"
-        CXX_STANDARD 17
-        ARGS ${_dummy}
-  )
-  if(${PROJECT_NAME}_symlink_code EQUAL 0)
-    set(${PROJECT_NAME}_WIN32_SYMLINK false CACHE BOOL "MinGW doesn't need workaround")
-    message(CHECK_PASS "OK")
-  else()
-    message(CHECK_FAIL "applying workaround")
-    set(${PROJECT_NAME}_WIN32_SYMLINK true CACHE BOOL "MinGW needs workaround")
-  endif()
-endif()
+include(${CMAKE_CURRENT_LIST_DIR}/FScheck.cmake)
+fs_check()
 
 endif(HAVE_CXX_FILESYSTEM)
 
--- test/permissions/test_permissions.cpp
+++ test/permissions/test_permissions.cpp
@@ -2,13 +2,18 @@
 #include <cstdlib>
 #include <string>
 
-#include <exception>
-
 #include "ffilesystem.h"
 
 #ifdef _MSC_VER
 #include <crtdbg.h>
 #endif
+
+
+[[noreturn]] void err(std::string_view m){
+    std::cerr << "ERROR: test_exe: " << m << "\n";
+    std::exit(EXIT_FAILURE);
+}
+
 
 int main()
 {
@@ -24,7 +29,7 @@ int main()
 
 std::string p = Ffs::get_permissions("");
 if(p.length() != 0)
-    throw std::runtime_error("test_exe: get_permissions('') should be empty, got: " + p);
+    err("get_permissions('') should be empty, got: " + p);
 
 std::string read = "readable.txt", noread = "nonreadable.txt", nowrite = "nonwritable.txt";
 
@@ -35,16 +40,16 @@ p = Ffs::get_permissions(read);
 std::cout << "Permissions for " << read << ": " << p << "\n";
 
 if(p.length() == 0)
-    throw std::runtime_error("test_exe: get_permissions('" + read + "') should not be empty");
+    err("get_permissions('" + read + "') should not be empty");
 
 if(!Ffs::is_readable(read))
-    throw std::runtime_error("test_exe: " + read + " should be readable");
+    err(read + " should be readable");
 
 if(!Ffs::exists(read))
-    throw std::runtime_error("test_exe: " + read + " should exist");
+   err(read + " should exist");
 
 if(!Ffs::is_file(read))
-    throw std::runtime_error("test_exe: " + read + " should be a file");
+    err(read + " should be a file");
 
 // for Ffilesystem, even non-readable files "exist" and are "is_file"
 Ffs::touch(noread);
@@ -54,17 +59,17 @@ p = Ffs::get_permissions(noread);
 std::cout << "Permissions for " << noread << ": " << p << "\n";
 
 if(p.length() == 0)
-    throw std::runtime_error("test_exe: get_permissions('" + noread + "') should not be empty");
+    err(noread + "') should not be empty");
 
 if(Ffs::is_readable(noread)){
     std::cerr << "XFAIL: test_exe: " << noread << " should not be readable\n";
 } else {
 
 if(!Ffs::exists(noread))
-    throw std::runtime_error("test_exe: " + noread + " should exist");
+    err(noread + " should exist");
 
 if(!Ffs::is_file(noread))
-    throw std::runtime_error("test_exe: " + noread + " should be a file");
+    err(noread + " should be a file");
 }
 // writable
 if(!Ffs::is_file(nowrite))
@@ -79,10 +84,10 @@ if(Ffs::is_writable(nowrite)){
 }
 
 if(!Ffs::exists(nowrite))
-    throw std::runtime_error("test_exe: " + nowrite + " should exist");
+    err(nowrite + " should exist");
 
 if(!Ffs::is_file(nowrite))
-    throw std::runtime_error("test_exe: " + nowrite + " should be a file");
+    err(nowrite + " should be a file");
 
 return EXIT_SUCCESS;
 }
